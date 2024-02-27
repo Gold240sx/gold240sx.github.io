0e960d52ea70dc3c32881f428f36e421c0eb4f5a
Gold240sx <240designworks@gmail.com>
2024-02-27 07:33:11 -0600

    project complete

---
+++ src/compute.js
@@ -0,0 +1,85 @@
+// Issue 1. Get the file content from the txt file
+const encodedMessage = [3, 6, 2, 4, 1, 5, 47, 29, 73, 49]
+
+// 1.1 - read the file contents
+const data = async () => {
+	const d = await fetch("./coding_qual_input.txt").then((response) => {
+		const formattedData = []
+		//1.2 - parse the file contents based on the line break (async then promise))
+		response.text().then((text) => {
+			text.split("\r\n").map((s) => {
+				// 1.3 - split the file contents based on the space
+				const segment = s
+				const [key, value] = segment.split(" ")
+				const numericVal = parseInt(key)
+				// 1.4 - push the formatted data to the global variable
+				formattedData.push({ numericVal, value })
+			})
+		})
+		return formattedData
+	})
+	return d
+}
+
+//1.5 - call the function to ensure data is returned from the async promise
+const outputData = async () => {
+	return await data()
+}
+
+/* 2nd issue: 
+	1. We need to sort the numbers from least to greatest, 
+	2. place them in lines(lineArray[])  (pyramid)
+*/
+//2.1
+const makePyramid = (numArr) => {
+	var arrLength = numArr.length
+	let sortedArr = numArr.sort((a, b) => a - b)
+	let pyramid = []
+	let i = 1
+	// the line length should first be set by the i
+	// then we push the value of the number to the line up to the length of the line
+	// then we set the newSortedArr === the sortedArr - the values we just used
+	// then we add 1 to i so long as i is less than the length of the sortedArr
+	while (arrLength) {
+		// with each new array, we push the values of the sorted array into the pyramid array
+		pyramid.push(sortedArr.splice(0, i))
+		arrLength = sortedArr.length // removes the used values so that the array length matches the desired pyramid length
+		i++
+	}
+	return pyramid
+}
+
+// 3rd issue, we need to pop off the ends of each line and create a new array from it:
+//3.1
+const endOfPyr = (pyr) => {
+	let endArr = []
+
+	pyr.map((arr) => {
+		let end = arr.pop()
+		endArr.push(end)
+	})
+	return endArr
+}
+//3.2 -  saving this as a variable to make this easier to read
+const endArr = endOfPyr(makePyramid(encodedMessage))
+
+//4th issue, we need to convert the numbers to the corresponding string from the data
+//4.1
+async function decodeDataToText(list, dataSource) {
+	// 4.2 - get the file content from the txt file
+	var message = await outputData().then((data) => {
+		// for each number in the endArr ...
+		const d = list.map((num) => {
+			// 4.3 - find the value that matches the number
+			const value = dataSource.find((d) => d.numericVal == num).value
+			return value
+		})
+		// add a space to the end of each word to form the sentence, and return the sentence
+		return d.join(" ")
+	})
+
+	return message
+}
+
+//Solution:
+console.log(decodeDataToText(endArr, data)) // Saved as a promise - easier to access in a project, and not just a vanilla JS file.
---
+++ src/ui.js
@@ -0,0 +1,139 @@
+const input = document.getElementById("current-number")
+const numPadContainer = document.getElementById("num-pad")
+const currentNumber = document.getElementById("current-number")
+const resetButton = document.getElementById("reset-btn")
+const endStringP = document.querySelector("#decoded-pop-up p")
+const nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ",", "<"]
+const numBtns = document.querySelectorAll(".numbtn")
+var currentNum = ""
+let currentNumArr = []
+var decodedMessage = ""
+
+for (let i = 0; i < nums.length; i++) {
+	let button = document.createElement("button")
+	button.classList = "numbtn"
+	button.type = "button"
+	button.onclick = () => numChange(nums[i])
+	if (i === 10) {
+		// adjust the position of the comma text
+		button.innerText = ","
+		button.style.paddingBottom = "10px"
+	} else {
+		button.innerText = nums[i]
+	}
+	numPadContainer.append(button)
+}
+let convertButton = document.createElement("button")
+convertButton.classList = "convertbtn"
+convertButton.type = "submit"
+convertButton.onclick = () => convertCode()
+convertButton.innerText = "Convert"
+numPadContainer.parentElement.append(convertButton)
+
+const numChange = (num) => {
+	if (num === "<") {
+		if (currentNum.slice(-2) == ", ") {
+			// if the last button pressed was a ", ", then remove both the comma and the space
+			currentNum = currentNum.slice(0, -2)
+		} else {
+			currentNum = currentNum.slice(0, -1)
+		}
+	} else if (num === ",") {
+		if (currentNum.slice(-2) == ", ") {
+			// dont add the comma if the last thing entered was a comma
+		} else {
+			currentNum = currentNum + ", "
+		}
+	} else {
+		currentNum += num
+	}
+	// if the number does not equal "" then show the reset button.
+	if (currentNum.trim() !== "") {
+		resetButton.style.opacity = "1"
+		resetButton.style.pointerEvents = "auto"
+	}
+
+	input.value = currentNum.toString()
+}
+
+const resetCurrentNum = () => {
+	currentNum = ""
+	input.value = ""
+	resetButton.style.opacity = "0"
+	resetButton.style.pointerEvents = "none"
+}
+
+const convertStringToArr = (str) => {
+	str.split(", ").map((s) => {
+		let value
+		if (typeof s !== "string") {
+			value = parseInt(s)
+		} else {
+			value = s
+		}
+		currentNumArr.push(value)
+	})
+	return currentNumArr
+}
+
+const showEndString = () => {
+	endStringP.innerText = decodedMessage
+	endStringP.parentElement.style.display = "flex"
+	let mindblown = document.createElement("div")
+	let mindblownText = document.createElement("p")
+	let dontWorryText = document.createElement("p")
+	let timer = document.createElement("p")
+	let timerCount = 5000
+	dontWorryText.innerText = "(Don't worry, it's just a joke.)"
+	dontWorryText.style.fontSize = "14px"
+	mindblownText.innerText = "🤯"
+	mindblown.style.display = "flex"
+	mindblown.style.justifyitems = "center"
+	mindblown.style.gap = "10px"
+	mindblown.style.backgroundColor = "red"
+	mindblownText.style.marginLeft = "20px"
+	// timer.style.fontSize = "14px"
+	mindblown.id = "mindblown"
+	mindblown.appendChild(timer)
+	mindblown.appendChild(dontWorryText)
+	endStringP.parentElement.appendChild(mindblown)
+	mindblown.appendChild(timer)
+	mindblown.appendChild(mindblownText)
+	// set timeout to hide the end string after 5 seconds
+
+	setInterval(() => {
+		if (timerCount === 0) {
+			timer.innerText = "0"
+			return
+		}
+		timerCount -= 1000
+		timer.innerText = `${timerCount / 1000} `
+	}, 1000)
+
+	// hide the message after x seconds
+	setTimeout(() => {
+		endStringP.parentElement.style.display = "none"
+		mindblown.innerHTML = ""
+	}, 5000)
+}
+
+const convertCode = async () => {
+	if (input.value.trim() === "") {
+		alert("Please enter a series of numbers to convert")
+		return
+	}
+	let endString = convertStringToArr(input.value)
+	console.log("endString", endString)
+	const data = await outputData()
+	// console.log(data)
+	const pyramid = makePyramid(endString)
+	console.log(pyramid)
+	const endPyramid = endOfPyr(pyramid)
+	// console.log("endPyramid", endPyramid)
+	decodedMessage = await decodeDataToText(endPyramid, data).then(
+		(message) => message
+	)
+	showEndString()
+	// console.log("decoded", decodedMessage)
+	resetCurrentNum()
+}
